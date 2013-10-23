b9c49dd67d01243e1f3bd67c1bc83114b7083877
Vitaly Litvinov <vl@transactpro.lv>
2013-10-23 10:36:28 +0300

    nodes improve & bugfix

--- common/forms/node.js
+++ common/forms/node.js
@@ -5,8 +5,7 @@ $a.click(function(){
 	if($ul.hasClass('expanded')){
 		$ul.removeClass('expanded').addClass('collapsed');
 		$(this).removeClass('collapse').addClass('expand');
-	} else
-	if($ul.hasClass('collapsed')){
+	} else if($ul.hasClass('collapsed')){
 		$ul.removeClass('collapsed').addClass('expanded');
 		$(this).removeClass('expand').addClass('collapse');
 		
@@ -15,4 +14,5 @@ $a.click(function(){
 			criteria:''
 		}, function(res){$ul.embed(res, true); console.log(res)});
 	}
+    $('.searchNodesController').find('ul.items').eq(0).removeClass('collapsed').addClass('expanded');
 });
