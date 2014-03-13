9a029f2b337f31d1e516473ac201e63f7999fa44
vl <vl@vl-VirtualBox.(none)>
2014-03-13 15:57:55 +0200

    Oxygen_Communication_Broadcast added

---
+++ communication/broadcast/broeadcast.class.php
@@ -0,0 +1,45 @@
+<?
+	class Oxygen_Communication_Broadcast {
+		private $url;
+		private $channel = false;
+
+		public function __construct($url, $channel = false){
+			$this->url = $url;
+			if($channel !== false){
+				$this->channel = $channel;
+			}
+		}
+
+		public function publish($channel, $data = false){
+			if(is_array($channel)){
+				if($this->channel === false){
+					throw new Exception("No channel has been set for broadcast server ".$this->url);
+				}else{
+					$data = $channel;
+				}
+			}else{
+				$this->channel = $channel;
+			}
+
+			$broadcastMessage = array(
+                "channel" => $this->channel,
+                "data" => $data
+            );
+			
+			$broadcastMessage = json_encode($broadcastMessage);
+
+			$curl = curl_init();
+			$headers = array(
+				"Content-Type: application/json",
+				"Content-length: ".strlen($broadcastMessage)
+			);
+            curl_setopt($curl, CURLOPT_URL, $this->url);
+            curl_setopt($curl, CURLOPT_HEADER, 0);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $broadcastMessage);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_exec($curl);
+            curl_close($curl);
+		}
+	}
