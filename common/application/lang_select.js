aebd6080ef5bd7ccb3222c393ef2d6521fc48aa0
vl <vl@tpro.lv>
2014-11-11 14:30:13 +0200

    cs

--- common/application/lang_select.js
+++ common/application/lang_select.js
@@ -1,13 +1,21 @@
 $this.find(".selectLang").click(function(){
 	var language = $(this).data("lang");
-	$this.remote("selectLanguege", {lang: language, page: location.href}, function(err, res){
-		if(err){
-			console.log(err);
-		}else{
-            location.href=res;
-			if(location.href == res){
-				window.location.reload();
+	$this.remote(
+		"selectLanguege", 
+		{
+			lang: language, 
+			page: location.href
+		}, 
+		function(err, res){
+			if(err){
+				console.log(err);
+			}else{
+				if(location.href == res){
+					window.location.reload();
+				}else{
+					location.href = res;
+				}			
 			}
 		}
-	});
+	);
 });
